c0a5fd0dc39e5b847e58f50469111cb896e4e1fb
l2lee <l2lee@ucsd.edu>
2022-11-19 20:13:44 -0800

    finish explore code

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # Lab 8 - Starter
 
 1) Where would you fit your automated tests in your Recipe project developement pipeline? Select one of the following and explain why?
-- 1. Within a Github action that runs whenever code is pushed. This is because whenever we want push code onto our repo, we want to make sure that the changes made have at least some sort of quality. This is basically a sanity check to ensure that your changes are at least not screwing up some existing functionalities. 
+- Within a Github action that runs whenever code is pushed. This is because whenever we want push code onto our repo, we want to make sure that the changes made have at least some sort of quality. This is basically a sanity check to ensure that your changes are at least not screwing up some existing functionalities. 
 
 2) Would you use an end to end test to chek if a funciton is returning the correct output?
-- No. This is more along the lines of unit test.
+- No. Checking the function of an output is more along the lines of unit testing.
+
+3) Would you use a unit test to test the "message" feature of a messaging application? Why or why not? For this aquestion, assumt the "message" feature allows a user to write and send a message to another user.
+- ?
+
+4) Would you use a unit test to test the "max message length" feature of a messaging application? Why or why not? For this question, assume the "max message length" feature prevents the user from typing more than 80 characters.
+- ?
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,111 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+// Testing isPhoneNumber
+test('isPhoneNumber 3-3-4 pattern', () => {
+    let number = '111-222-3333';
+    expect(functions.isPhoneNumber(number)).toBe(true);
+});
+
+test('isPhoneNumber san diego number', () => {
+    let number = '858-302-8490';
+    expect(functions.isPhoneNumber(number)).toBe(true);
+});
+
+test('isPhoneNumber empty number', () => {
+    let number = '';
+    expect(functions.isPhoneNumber(number)).toBe(false);
+});
+
+test('isPhoneNumber another country number', () => {
+    let number = '0739-833-892';
+    expect(functions.isPhoneNumber(number)).toBe(false);
+});
+
+// Testing isEmail
+test('isEmail khosla', () => {
+    let email = 'khosla@ucsd.edu';
+    expect(functions.isEmail(email)).toBe(true);
+});
+
+test('isEmail prof', () => {
+    let email = 'tpowell2@ucsd.edu';
+    expect(functions.isEmail(email)).toBe(true);
+});
+
+test('isEmail empty email', () => {
+    let email = '';
+    expect(functions.isEmail(email)).toBe(false);
+});
+
+test('isEmail no @', () => {
+    let email = 'khosla.ucsd.edu';
+    expect(functions.isEmail(email)).toBe(false);
+});
+
+// Testing isStrongPassword
+test('isStrongPassword sameletters true', () => {
+    let password = 'aaaa';
+    expect(functions.isStrongPassword(password)).toBe(true);
+});
+
+test('isStrongPassword 15 digits true', () => {
+    let password = 'a23456789012345';
+    expect(functions.isStrongPassword(password)).toBe(true);
+
+});
+
+test('isStrongPassword does not start with lettter', () => {
+    let password = '1aaa';
+    expect(functions.isStrongPassword(password)).toBe(false);
+});
+
+test('isStrongPassword over 15 digits', () => {
+    let password = '1234567890123456';
+    expect(functions.isStrongPassword(password)).toBe(false);
+});
+
+// Testing isDate
+test('isDate Jan 1st 2020', () => {
+    let date = '1/1/2020';
+    expect(functions.isDate(date)).toBe(true)
+});
+
+test('isDate Dec 31st 2020', () => {
+    let date = '12/31/2020';
+    expect(functions.isDate(date)).toBe(true)
+});
+
+test('isDate 2-digit year', () => {
+    let date = '12/31/20';
+    expect(functions.isDate(date)).toBe(false)
+});
+
+test('isDate non-numeric', () => {
+    let date = 'a/w/2020';
+    expect(functions.isDate(date)).toBe(false)
+});
+
+// Testing isHexColor
+test('isHexColor with hashtag', () => {
+    let color = '#ffffff';
+    expect(functions.isHexColor(color)).toBe(true)
+});
+
+test('isHexColor without hashtag', () => {
+    let color = 'ffffff';
+    expect(functions.isHexColor(color)).toBe(true)
+});
+
+test('isHexColor less than 3 digits', () => {
+    let color = 'ff';
+    expect(functions.isHexColor(color)).toBe(false);
+});
+
+test('isHexColor random characters', () => {
+    let color = 'ff_#f0';
+    expect(functions.isHexColor(color)).toBe(false);
+});
+
+
+
